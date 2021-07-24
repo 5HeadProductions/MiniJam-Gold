9be814352a60e759d11a12de3e20804341ccbaed
Andres Maturin <idontknowwhattonamethiskhay@gmail.com>
2021-07-24 09:41:03 -0700

    Falling coins fall
    Crated 3 scripts, one for the spawner, the falling coin, and the object pooler

--- MiniJam/Assets/Scripts/FallingCoin.cs
+++ MiniJam/Assets/Scripts/FallingCoin.cs
@@ -2,23 +2,24 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class FallingCoin : MonoBehaviour, IPooledObject
+public class FallingCoin : MonoBehaviour
 {
-
-    public float sideForce = -7f;
-    public float upforce = .1f;
-    public GameObject spawner;
-    // Start is called before the first frame update
-    public void OnObjectSpawn()
+    public void OnEnable()
     {
-        spawner = GameObject.Find("FallingSpawner");
-        float xForce = Random.Range(-sideForce, sideForce);
-        float yForce = Random.Range(upforce/2f, upforce);
-        Debug.Log(xForce.ToString());
-        Vector2 force = new Vector2 (xForce, yForce);
+        Invoke("Disable", 2f);
+    }
 
-        GetComponent<Rigidbody2D>().velocity = force;
+    public void Disable()
+    {
+        gameObject.SetActive(false);
+    }
 
+    public void OnDisable()
+    {
+        CancelInvoke();
+    }
+    public void OnCollisionEnter2D(Collision2D col){
+        if(col.gameObject.tag == "Player") Debug.Log("hit");
     }
     
 }
--- MiniJam/Assets/Scripts/ObjectPooler.cs
+++ MiniJam/Assets/Scripts/ObjectPooler.cs
@@ -4,65 +4,33 @@ using UnityEngine;
 
 public class ObjectPooler : MonoBehaviour
 {
-    [System.Serializable]
-    public class Pool
-    {
-        public string tag;
-        public GameObject prefab;
-
-        public int size;
-    }
 
     public static ObjectPooler Instance;
-    private void Awake()
-    {
-        Instance = this;
-    }
+    public GameObject pooledObject;
+    public int size;
 
-    public List<Pool> pools;
-    public Dictionary<string, Queue<GameObject>> poolDictionary;
+    private List<GameObject> pooledObjects;
+
     // Start is called before the first frame update
     void Start()
     {
-
-        poolDictionary = new Dictionary<string, Queue<GameObject>>();
-
-        foreach(Pool pool in pools){
-            Queue<GameObject> objectsPool = new Queue<GameObject>();
-
-            for(int i = 0; i < pool.size; i++){
-                GameObject obj = Instantiate(pool.prefab);
-                obj.SetActive(false);
-                objectsPool.Enqueue(obj);
-            }
-
-            poolDictionary.Add(pool.tag, objectsPool);
+        Instance = this;
+        pooledObjects = new List<GameObject>();
+        for(int i = 0; i < size; i++){
+            GameObject obj = Instantiate(pooledObject);
+            obj.SetActive(false);
+            pooledObjects.Add(obj);
         }
         
     }
-
-    public GameObject SpawnFromPool(string tag, Vector2 position, Quaternion rotation){
-
-
-        if(!poolDictionary.ContainsKey(tag)){
-            Debug.LogWarning("Pool with tag " + tag + " doesn't exist");
-            return null;
-        }
-        GameObject objectToSpawn = poolDictionary[tag].Dequeue();
-
-        objectToSpawn.SetActive(true);
-        objectToSpawn.transform.position = position;
-        objectToSpawn.transform.rotation = rotation;
-
-
-        IPooledObject pooledObj = objectToSpawn.GetComponent<IPooledObject>();
-        if(pooledObj != null){
-            pooledObj.OnObjectSpawn();
+    public GameObject GetPooledObject(){
+        for(int i = 0; i < pooledObjects.Count; i++){
+            if(!pooledObjects[i].activeInHierarchy){
+                return pooledObjects[i];
+            }
         }
 
-        poolDictionary[tag].Enqueue(objectToSpawn);
-
-        return objectToSpawn;
+        return null;
     }
 
     
